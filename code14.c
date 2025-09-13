452cb76cff159a189a527a4a0bf9830e7939e943
Ridhi443 <ridhigulati156@gmail.com>
2025-09-13 23:56:09 +0530

    Add files via upload

---
+++ code14.c
@@ -0,0 +1,20 @@
+// write a program to input a character and check whether it is a vowel or consonant not using if-else. 
+#include <stdio.h>
+int main() 
+{
+   char ch;
+    printf("enter the character: ");
+    scanf("%c",&ch);
+    if(( ch>='a' && ch<='z')) 
+    { 
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        {
+        printf("it is a vowel");
+    }
+    else 
+   {
+    printf("it is a consonant");
+}
+}
+return 0;
+}
